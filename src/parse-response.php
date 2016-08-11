a3ca2c870ccd7690117cbde39b4a177bec134255
AJ Michels <amichels@caringbridge.org>
2016-08-11 15:24:28 -0500

    Add script for parsing a SAML Response

---
+++ src/parse-response.php
@@ -0,0 +1,27 @@
+#!/usr/bin/env php
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$stdin = file_get_contents('php://stdin');
+$_POST['SAMLResponse'] = $stdin;
+
+// print(base64_decode($stdin) . "\n\n\n");
+
+$spString = 'https://hmki_sso_nonprod.hallmarkinsights.com';
+$spConfig = new \SAML2\COnfiguration\ServiceProvider([
+    'CertificateFile' => __DIR__ . '/../certs/example-tp.org.crt',
+]);
+$idpConfig = new \SAML2\COnfiguration\IdentityProvider([
+    'CertificateFile' => __DIR__ . '/../certs/example.org.crt',
+]);
+$destination = new \SAML2\Configuration\Destination($spString);
+
+\SAML2\Compat\ContainerSingleton::setContainer(new \SAML2\Compat\MockContainer());
+
+$httpPost = new \SAML2\HTTPPost();
+$response = $httpPost->receive();
+$processor = new \SAML2\Response\Processor(new \Psr\Log\NullLogger());
+$assertion = $processor->process($spConfig, $idpConfig, $destination, $response);
+
+print($assertion->toXml());
